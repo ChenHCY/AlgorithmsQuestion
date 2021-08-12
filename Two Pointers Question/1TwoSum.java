3e6754bd78dd0b5d3a607a5861a982c42c02dc88
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2021-08-12 16:22:22 -0700

    Update 1TwoSum.java

--- Two Pointers Question/1TwoSum.java
+++ Two Pointers Question/1TwoSum.java
@@ -16,6 +16,8 @@ Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 */
+
+// Leetcode:
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] helper = Arrays.copyOf(nums, nums.length); // creative a new list to find which number
@@ -44,6 +46,45 @@ class Solution {
                 x[1] = i;
             }
         }
+        return x;
+    }
+}
+
+// Lintcode 56 · Two Sum
+public class Solution {
+    /**
+     * @param numbers: An array of Integer
+     * @param target: target = numbers[index1] + numbers[index2]
+     * @return: [index1, index2] (index1 < index2)
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        // write your code here
+        int[] helper = Arrays.copyOf(numbers, numbers.length); // creative a new list to find which number
+        Arrays.sort(helper); // let the list be a array list n
+        
+        int left = 0;
+        int right = helper.length - 1;
+        int x[] = new int[2]; // the result
+        x[0] = -1;
+
+        while(left < right){
+            if(helper[left] + helper[right] > target){
+                right--;
+            } else if (helper[left] + helper[right] < target){
+                left++;
+            } else {
+                break;
+            }
+        }
+
+        for(int i = 0; i < numbers.length; i++){
+            if(x[0] == -1 && numbers[i] == helper[left]){
+                x[0] = i;
+            } else if(numbers[i] == helper[right]){
+                x[1] = i;
+            }
+        }
+        Arrays.sort(x);
         return x;
     }
 }
