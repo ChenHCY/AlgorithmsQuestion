4f92012288fb0f9655df67bda51dbc75ff6fcf51
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2021-08-13 17:05:11 -0700

    Create 344ReverseString.java

---
+++ Two Pointers Question/344ReverseString.java
@@ -0,0 +1,31 @@
+/* 344. Reverse String
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        char temp;
+        while(left < right){
+            if(left != right){
+                temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
+ 
