acdc42be744e800070939d55d96b7368c1e0a57e
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-06-25 15:39:26 -0700

    Create 104 Maximum Depth of Binary Tree.java

---
+++ Leetcode Question/104 Maximum Depth of Binary Tree.java
@@ -0,0 +1,44 @@
+/* Leetcode 104. Maximum Depth of Binary Tree
+
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        
+        int result;
+        int lnum = maxDepth(root.left);
+        int rnum = maxDepth(root.right);
+        
+        result = Math.max(lnum, rnum)+1;
+        return result;
+    }
+
+}
