f453b5dd51ad53e9db63a50ba80ab6bdd911ef01
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-07-22 18:55:02 -0700

    Create 409 Longest Palindrome.java

---
+++ Leetcode Question/409 Longest Palindrome.java
@@ -0,0 +1,55 @@
+/*Leetcode 409. Longest Palindrome
+
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+ 
+Example 1:
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+Example 2:
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1.
+ 
+
+Constraints:
+1 <= s.length <= 2000
+s consists of lowercase and/or uppercase English letters only.
+*/
+
+class Solution {
+    public int longestPalindrome(String s) {
+        if(s.length() < 1 || s == null){
+            return 0;
+        }
+        int length = s.length();
+        int res = 0;
+        
+        //HashSet did not allowed have same number
+        HashSet<Character> set = new HashSet<>();
+        
+        //Traverse every character from String s and try to find singular letters
+        for(char c : s.toCharArray()){
+            if(set.contains(c)){ //if hashset ready have curr character
+                set.remove(c); // remove curr characrer from hashset
+            } else{
+                set.add(c); // if not, add in hashset => finally will get all singular letters
+            }
+        }
+        
+        //if we want to find the Longest Palindrome, the palindrome consists of letters with equal partners, plus possibly a unique center 
+        if(set.size() == 0){ 
+            //if there is did not have any singular letters, the longest Palindrome is the Length of double-digit letters
+            res = length - set.size(); 
+        } else{
+             //if there is any singular letters, the longest Palindrome is the Length of double-digit letters and plue one (unique center)
+            res = length - set.size() + 1;
+        }
+        
+        return res;
+        
+    }
+}
