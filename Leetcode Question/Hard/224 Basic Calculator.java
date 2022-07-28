69edf4a4424085157d7e6426aed341eb4403396e
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-07-28 00:35:39 -0700

    Update 224 Basic Calculator.java

--- Leetcode Question/Hard/224 Basic Calculator.java
+++ Leetcode Question/Hard/224 Basic Calculator.java
@@ -30,29 +30,29 @@ class Solution {
     public int calculate(String s) {
         int res = 0;
         int numberSign = 1; // the number sign
-        Stack<Integer> stack = new Stack<Integer>();
+        Stack<Integer> stack = new Stack<Integer>(); //Use Stack<> to storing and distinguishing between numbers inside and outside parentheses'()'
         
         for(int i = 0; i < s.length(); i++){
-            if(Character.isDigit(s.charAt(i))){
+            if(Character.isDigit(s.charAt(i))){ //isDigit() is used for determines whether a specified character is a digit or not. 
                 int sum = s.charAt(i) - '0'; //change the string format into integer
                 while(i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))){ //get the every whole number 
-                    sum = sum * 10 + s.charAt(i + 1) - '0';
-                    //sum += s.charAt(i + 1) - '0';
-                    i++;
+                    sum = sum * 10 + s.charAt(i + 1) - '0';//Need to get correct whole number value, the prev digit is tens
+                    i++;//move i-pointer into next digit
                 }
-                res += sum * numberSign;
+                res += sum * numberSign; //need care this current number is negative or positive
             } else if(s.charAt(i) == '+'){ //when get the sign from String s is '+'
                 numberSign = 1;
             } else if(s.charAt(i) == '-'){ //when get the sign from String s is '-'
                 numberSign = -1;
             } else if(s.charAt(i) == '('){ //The parentheses'()' have higher priority
-                stack.push(res);
+                stack.push(res); // push(E element) method is used to push an element into the Stack. 
                 stack.push(numberSign);
-                res = 0;
-                numberSign = 1;
+                res = 0; // So both of "res" and "numberSign" have to be reset to their starting values (res=, numberSign=1)
+                numberSign = 1; //and then start calculating the sum inside the parentheses.
             } else if(s.charAt(i) == ')'){
                 res = res * stack.pop() + stack.pop();
-            }
+            } // The pop() function is used to remove or 'pop' an element from the top of the stack(newest or the topmost element in the stack).
+              // so first "stack.pop()" is number Sign, second is the number before parentheses'()'
         }
         return res;
    }
