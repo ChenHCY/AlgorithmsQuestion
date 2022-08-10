de1c29629d051b075458baffedc1850e6a0df92c
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-08-09 22:07:41 -0700

    Create 496 Next Greater Element I.java

---
+++ Leetcode Question/496 Next Greater Element I.java
@@ -0,0 +1,62 @@
+/* Leetcode 496. Next Greater Element I
+
+The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next 
+greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+ 
+Example 1:
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+- 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+- 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+Example 2:
+Input: nums1 = [2,4], nums2 = [1,2,3,4]
+Output: [3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+- 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+ 
+
+Constraints:
+1 <= nums1.length <= nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 104
+All integers in nums1 and nums2 are unique.
+All the integers of nums1 also appear in nums2.
+*/
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[nums1.length];
+        int index = 0;
+        
+        //save the nums2 number into hashmap with their postion
+        for(int i = 0; i < nums2.length; i++){
+            map.put(nums2[i], i); 
+        }
+        
+        for(int j = 0; j < nums1.length; j++){
+            res[j] = -1; //  If there is no next greater element, then the answer for this query is -1
+            int start = map.get(nums1[j]); //find the where is nums1 element value postion in nums2 list
+            
+            //when there is have next greater element in nums2 list, 
+            // change nums1[current] value equals next greater element from nums2 list
+            for(int k = start; k < nums2.length; k++){
+                if(nums1[j] < nums2[k]){
+                    res[j] = nums2[k];
+                    break;
+                }
+            }
+            
+        }
+        
+        return res;
+    }
+}
