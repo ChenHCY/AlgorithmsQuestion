1d367956939b66d2a31bdda5a3406f025373b1a4
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-08-12 14:27:30 -0700

    Update 13 Roman to Integer.java

--- Leetcode Question/13 Roman to Integer.java
+++ Leetcode Question/13 Roman to Integer.java
@@ -20,6 +20,17 @@ I can be placed before V (5) and X (10) to make 4 and 9.
 X can be placed before L (50) and C (100) to make 40 and 90. 
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
+
+Example 1:
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+
+Example 2:
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
 */
 
 class Solution {
