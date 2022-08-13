e08b497dcc5a0c4ed67485737ea659b0a2ac182c
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-08-12 23:28:51 -0700

    Update 14FirstPositionofTarget.java

--- Binary Search Question/14FirstPositionofTarget.java
+++ Binary Search Question/14FirstPositionofTarget.java
@@ -31,18 +31,21 @@ public class Solution {
         int left = 0;
         int right = nums.length - 1;
 
-        while (left <= right){
-            int mid = (left + right) / 2;
-            if (nums[mid] < target){
+        while(left < right){
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] == target){
+                right = mid;
+            } else if(nums[mid] < target){
                 left = mid + 1;
-            } else if(nums[mid] >= target){
+            } else{
                 right = mid - 1;
             }
         }
-        
-        if(target == nums[left] && left <= nums.length){
-            return left;
+
+        if(nums[right] == target){
+            return right;
         }
+
         return -1;
     }
 }
