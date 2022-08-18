a41a1ec1f7e3164076899959c8760d1e94b205a1
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-08-18 14:42:42 -0700

    Update OA 1 Question 1.java

--- Amazon OA/OA 1 Question 1.java
+++ Amazon OA/OA 1 Question 1.java
@@ -5,7 +5,7 @@ Example:
 Input: "xyxy"
 Output: "xxyy"
 */
-
+//Solution 1: Used bucket to sort and save element
 public class MyClass {
     public static void main(String args[]) {
       String str = "xyxy";
@@ -34,6 +34,48 @@ public class MyClass {
                     }
                 } else{ //if the number of current character is odd
                     for(int z = 0; z < array[i]; z++){
+                        mid.append((char) (i + 'a')); //add all the number of current characters into mid Part
+                    }
+                }
+            }
+        }
+        //Output be palindrome format
+        return firstPart.toString() + mid.toString() + firstPart.reverse().toString();
+        
+    }
+}
+
+//Solution 2: Used HashMap to save every number of each character from string
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyClass {
+    public static void main(String args[]) {
+      String str = "abbbaaa";
+      System.out.println(reverse(str));
+    }
+      
+    public static String reverse(String s){
+        HashMap<Character, Integer> map = new HashMap<>();
+        StringBuilder firstPart = new StringBuilder();
+        StringBuilder mid = new StringBuilder();
+        
+      //Save all the character element from String s into hashmap
+        for(int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1); // Count the numbers of each character from String
+        }
+        
+      //Use for-loop to sort String to be palindrome 
+        for(int i = 0; i < 26; i++){
+            if(map.containsKey((char) (i + 'a'))){ //need check whether have this character element
+                int number = map.get((char) (i + 'a')); //how many number of curr element
+                if(number % 2 == 0){ //if the number of current character is even
+                    for(int k = 0; k < (number / 2); k++){
+                        firstPart.append((char) (i + 'a')); //add half part of current character into firstPart StringBuilder
+                    }
+                } else{ //if the number of current character is odd
+                    for(int z = 0; z < number; z++){
                         mid.append((char) (i + 'a')); //add all the number of current characters into mid Part
                     }
                 }
