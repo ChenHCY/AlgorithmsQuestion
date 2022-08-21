208b97701bfe5bbb76577580cc038745c2adebd1
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-08-20 17:58:43 -0700

    Create OA 1 Question 1.java

---
+++ Roblox OA/OA 1 Question 1.java
@@ -0,0 +1,31 @@
+/* Roblox OA Question 1
+
+Count All the numbers that are not equal to first or second number of array
+
+Example: Input: int[] nums = [3, 4, 1, 1, 3, 1];
+         Output: 3
+*/
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 4, 1, 1, 3, 1};
+        System.out.println(solution(nums));
+    }
+    
+    public static int solution(int[] nums){
+        if(nums.length <= 1){
+            return 0;
+        }
+        
+        int res = 0;
+        //use a for-loop traverse all the element from nums list
+        //and check whether equals first or second number
+        for(int i = 2; i < nums.length; i++){
+            if(nums[i] != nums[0] && nums[i] != nums[1]){
+                res += 1;
+            }
+        }
+        
+        return res;
+    }
+}
