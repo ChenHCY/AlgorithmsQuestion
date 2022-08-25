0cdab9a6cb06fcba7fb184cd42a58ae8d46794f8
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-08-25 00:10:24 -0700

    Update 501 Find Mode in Binary Search Tree.java

--- HashMap Question/501 Find Mode in Binary Search Tree.java
+++ HashMap Question/501 Find Mode in Binary Search Tree.java
@@ -1,8 +1,6 @@
 /*Leetcode 501. Find Mode in Binary Search Tree
 Given the root of a binary search tree (BST) with duplicates, return all the mode(s) (i.e., the most frequently occurred element) in it.
-
 If the tree has more than one mode, return them in any order.
-
 Assume a BST is defined as follows:
 
 The left subtree of a node contains only nodes with keys less than or equal to the node's key.
@@ -25,6 +23,7 @@ The number of nodes in the tree is in the range [1, 104].
 
 Follow up: Could you do that without using any extra space? (Assume that the implicit stack space incurred due to recursion does not count).
 */
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -41,21 +40,22 @@ Follow up: Could you do that without using any extra space? (Assume that the imp
  * }
  */
 class Solution {
-    Map<Integer, Integer> map;
+    Map<Integer, Integer> map; //<Key, Value>
     int max = 0;
     public int[] findMode(TreeNode root) {
         if(root == null){
             return new int[0];
         }
         this.map = new HashMap<>(); //Used HashMap to count the most frequently occurred times 
+        inOrder(root); //call the in-order function
         
-        inOrder(root);
+        //Used arrayList to save all the most frequently occurred element
+        List<Integer> list = new ArrayList<>(); 
         
-        List<Integer> list = new ArrayList<>();
-        
+        //Travser all the element from root tree through HashMap
         for(int key : map.keySet()){
-            if(map.get(key) == max){
-                list.add(key); //add the most frequently occurred element into ArrayList
+            if(map.get(key) == max){ //if curr element occurred element equals max times
+                list.add(key); //add curr element into ArrayList
             }
         }
         
@@ -68,16 +68,16 @@ class Solution {
         return res;
     }
     
-    //In-order function:
+    //In-order function: Use the In-order to add all the element into hashmap with occurred  times
     public void inOrder(TreeNode node){
         if(node.left != null){
-            inOrder(node.left);
+            inOrder(node.left); //add node left side into HashMap
         }
-        //Used HashMap to count the most frequently occurred times 
-        map.put(node.val, map.getOrDefault(node.val, 0) + 1);
+        //Used HashMap to count the occurred times of every number elements
+        map.put(node.val, map.getOrDefault(node.val, 0) + 1); 
         max = Math.max(max, map.get(node.val)); // find the most frequently occurred element
         if(node.right != null){
-            inOrder(node.right);
+            inOrder(node.right);//add node right side into HashMap
         }
     }
 }
