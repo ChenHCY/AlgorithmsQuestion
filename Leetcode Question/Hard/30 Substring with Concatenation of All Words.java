0f99661b8930b2fc92ed7660092c5543d48dd416
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-09-16 00:12:23 -0700

    Create 30 Substring with Concatenation of All Words.java

---
+++ Leetcode Question/Hard/30 Substring with Concatenation of All Words.java
@@ -0,0 +1,89 @@
+/*Leetcode 30. Substring with Concatenation of All Words
+
+You are given a string s and an array of strings words. All the strings of words are of the same length.
+A concatenated substring in s is a substring that contains all the strings of any permutation of words concatenated.
+
+For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are 
+all concatenated strings. "acdbef" is not a concatenated substring because it is not the concatenation of any permutation of words.
+
+Return the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+
+Example 1:
+Input: s = "barfoothefoobarman", words = ["foo","bar"]
+Output: [0,9]
+Explanation: Since words.length == 2 and words[i].length == 3, the concatenated substring has to be of length 6.
+The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+The output order does not matter. Returning [9,0] is fine too.
+
+Example 2:
+Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+Output: []
+Explanation: Since words.length == 4 and words[i].length == 4, the concatenated substring has to be of length 16.
+There is no substring of length 16 is s that is equal to the concatenation of any permutation of words.
+We return an empty array.
+
+Example 3:
+Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+Output: [6,9,12]
+Explanation: Since words.length == 3 and words[i].length == 3, the concatenated substring has to be of length 9.
+The substring starting at 6 is "foobarthe". It is the concatenation of ["foo","bar","the"] which is a permutation of words.
+The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"] which is a permutation of words.
+The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"] which is a permutation of words.
+ 
+
+Constraints:
+1 <= s.length <= 104
+1 <= words.length <= 5000
+1 <= words[i].length <= 30
+s and words[i] consist of lowercase English letters.
+*/
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        if(s == null || words == null){
+            return new ArrayList<>();
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        int len = words.length; //how many words in the list
+        int elementLen = words[0].length(); // the length of one words from words[] list
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        //save the element string from words list into HashMap
+        for(String str : words){
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        
+        //travser the string s
+        for(int i = 0; i <= s.length() - len * elementLen; i++){
+            int k = len; // use for Count the number element of words list
+            int j = i; //i and j is a sliding window to check whether the current words is in the HashMap
+            HashMap<String, Integer> copy = new HashMap<>(map);
+            
+            // "barfoothefoobarman"
+           //   i'       i'  
+          //    j' j' j  j' j  j
+            while(k > 0){
+                //Follow the example: strB = "bar"
+                String strB = s.substring(j, j + elementLen);
+                //check whether can not find the current words in the HashMap (words[] list)
+                //and whether the current words are repeated
+                if(!copy.containsKey(strB) || copy.get(strB) < 1){
+                    break;
+                }
+                //if can find current words in the HashMap (words[] list)
+                copy.put(strB, copy.get(strB) - 1);
+                k--;
+                j += elementLen;
+            }
+            //If can find all words in list order, so it a substring with Concatenation of All Words
+            //So save the i poistion into result list
+            if(k == 0){
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+}
