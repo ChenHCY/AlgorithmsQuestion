98d68378e560dc65911349023d09d4989da74432
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-10-01 17:12:59 -0700

    Update 41 First Missing Positive.java

--- Leetcode Question/Hard/41 First Missing Positive.java
+++ Leetcode Question/Hard/41 First Missing Positive.java
@@ -24,3 +24,42 @@ Constraints:
 -231 <= nums[i] <= 231 - 1
 */
 
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        //Time: O(n)  Space: O(1)
+        if(nums.length == 0 || nums == null){
+            return 1;
+        }
+        // Bucket sort idea: Exchange and Put every number in correct position
+        // then find which one is difference (nums[i] != i + 1)
+        /* [0, 1, 2, 3]
+           [1, 2, 3, 4] ==> nums[i] = i + 1
+           The bucket sort process:
+           [3, 1, 4, -1] ==> i = 0  nums[i] = 3  nums[nums[i] - 1] = 2 != 4 ==> so exchange 3 with 4
+           [4, 1, 3, -1] ==> i = 0  nums[i] = 4  nums[nums[i] - 1] = 3 != -1 ==> so exchange 4 with -1
+           [-1, 1, 3, 4] ==> nums[i] = -1 < 0 move to next step
+           [-1, 1, 3, 4] ==> i = 1 nums[i] = 1  nums[nums[i] - 1] = nums[0] = -1 != 1 ==> so exchange -1 with 1
+           Exchange and Put every number in correct position: [1, -1, 3, 4]
+        */
+        
+        //Traverse the number list[] to exchange that put every number in correct position
+        for(int i = 0; i < nums.length; i++){ 
+            //nums[nums[i] - 1] means the nums[i]'s correct position
+            while(nums[i] > 0 && nums[i] < nums.length && nums[nums[i] - 1] != nums[i]){
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        
+        //After Exchange, Traverse the nums[] list from front to back
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != j + 1){
+                return j + 1; //if find which number is not equal i + 1, it's First Missing Positive
+            }
+        }
+        
+        //Example [1, 2, 3, 4], the First Missing Positive is next number after nums list
+        return nums.length + 1; 
+    }
+}
