1f0b30862520d8e68d4539ddf4c130b53ab7019b
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-10-09 23:56:58 -0700

    Create 1181 Diameter of Binary Tree.java

---
+++ Amazon OA/Prepare VI Question/1181 Diameter of Binary Tree.java
@@ -0,0 +1,30 @@
+/* Lintcode 1181 · Diameter of Binary Tree  && 543. Diameter of Binary Tree
+
+Description
+Given a binary tree, you need to compute the length of the diameter of the tree. 
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
+
+Example 1:
+Given a binary tree 
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Example 2:
+Input:[2,3,#,1]
+Output:2
+Explanation:
+      2
+    /
+   3
+ /
+1
+
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+-100 <= Node.val <= 100
+*/
+
