c2b6e0d8c637d2df473cc58d793f7cbfd6ffc60c
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-10-17 10:39:04 -0700

    Create 215 Kth Largest Element in an Array.java

---
+++ Leetcode Question/Medium/215 Kth Largest Element in an Array.java
@@ -0,0 +1,35 @@
+/* Leetcode 215. Kth Largest Element in an Array
+Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+You must solve it in O(n) time complexity.
+
+Example 1:
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+
+Example 2:
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+
+Constraints:
+1 <= k <= nums.length <= 105
+-104 <= nums[i] <= 104
+*/
+//Used the PriorityQueue to sort the numbers in increasing order
+//Time: O(n)  Space: O(n)
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        // used the PriorityQueue to sort the numbers in increasing order
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+
+        // keep k largest elements in the heap
+        for (int n: nums) {
+          heap.add(n);
+          if (heap.size() > k)
+            heap.poll();
+        }
+
+        // output
+        return heap.poll();        
+  }
+}
