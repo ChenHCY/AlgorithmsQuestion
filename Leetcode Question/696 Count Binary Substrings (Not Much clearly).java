9c09a11b053aaf1eb65229f810fd53f919047d5b
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-10-26 22:57:57 -0700

    Create 696 Count Binary Substrings (Not Much clearly).java

---
+++ Leetcode Question/696 Count Binary Substrings (Not Much clearly).java
@@ -0,0 +1,42 @@
+/* Leetcode 696. Count Binary Substrings
+Given a binary string s, return the number of non-empty substrings that 
+have the same number of 0's and 1's, and all the 0's and all the 1's 
+in these substrings are grouped consecutively.
+
+Substrings that occur multiple times are counted the number of times they occur.
+Example 1:
+Input: s = "00110011"
+Output: 6
+Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+Notice that some of these substrings repeat and are counted the number of times they occur.
+Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+
+Example 2:
+Input: s = "10101"
+Output: 4
+Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.
+ 
+Constraints:
+1 <= s.length <= 105
+s[i] is either '0' or '1'.
+*/
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int res = 0;
+        int prev = 0;
+        int cur = 1;
+        
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i - 1) != s.charAt(i)){ //the cur element is not same with previous element
+                res += Math.min(prev, cur);
+                prev = cur;
+                cur = 1;
+            } else{ //there is same, move cur pointer into next element
+                cur++;
+            }
+        }
+        
+        res += Math.min(prev, cur);
+        return res;
+    }
+}
