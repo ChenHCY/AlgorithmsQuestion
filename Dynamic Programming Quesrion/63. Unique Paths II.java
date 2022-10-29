0473f42209096168862484db05bb7911c816d000
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-10-29 14:13:33 -0700

    Update 63. Unique Paths II.java

--- Dynamic Programming Quesrion/63. Unique Paths II.java
+++ Dynamic Programming Quesrion/63. Unique Paths II.java
@@ -26,4 +26,55 @@ n == obstacleGrid[i].length
 1 <= m, n <= 100
 obstacleGrid[i][j] is 0 or 1.
 */
-
+// Time Complexity: O(M*N)  Space: O(n) ==> declare and Used a new int[][] dp
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        //Used Dp Method:
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        
+         //base case: the top-left corner or the bottom-right corner is obstacle
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1){
+            return 0; //it means did not have any possible unique paths that the robot can arrived
+        }
+        
+        // i is row, j is colum
+        /* Dp function: 1. through first row or first colum
+                       dp[i][j] = dp[0][j] ==> i = 0 first row only one path
+                       dp[i][j] = dp[i][0] ==> j = 0 first colum only one path
+                       2. the robot can only move either down or right at any point in time.
+                       dp[i][j] = dp[i-1][j] ==> down direction
+                       dp[i][j] = dp[i][j-1] ==> right direction
+                       3. Meet the obstacle
+                       obstacleGrid[i][j] == 1 ==> Meet the obstacle
+        */
+        
+        // Dp state: represents the maximum number of paths 
+        int[][] dp = new int[m][n];
+        
+        //first row
+        for(int i = 0; i < m && obstacleGrid[i][0] == 0; i++){
+            dp[i][0] = 1;
+        }
+        
+        //first column
+        for(int j = 0; j < n && obstacleGrid[0][j] == 0; j++){
+            dp[0][j] = 1;
+        }
+        
+        
+        //traverse all the path solution:
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(obstacleGrid[i][j] == 1){ //meet the obstacle
+                    dp[i][j] = 0;
+                    continue;
+                } else{ //The robot can only move either down or right at any point in time.
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        
+        return dp[m-1][n-1]; // Dp answer: The robot tries to move to the bottom-right corner
+    }
+}
