066f8981b50f7792ed1753312157caa36ac3f60d
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-11-04 13:59:27 -0700

    Update OA 2 Question 4.java

--- Roblox OA/OA 2 Question 4.java
+++ Roblox OA/OA 2 Question 4.java
@@ -14,9 +14,15 @@ public class Main
 	}
 	
 	public static int[] solution(int[] houses, int[] queries) {
+	//base case ==> remove 1 
+	if(houses.length == 1){
+	    return new int[]{0};
+	}
+		
         List<Integer> list = new ArrayList<>();
         int segments = 1;
         HashMap<Integer, Integer> map = new HashMap<>();
+	Arrays.sort(houses);
     
         //get the how many segments of houses before queries
         for(int i = 1; i < houses.length; i++){
@@ -25,34 +31,36 @@ public class Main
             } 
         }
     
+	//save all the houses with position into the HashMap
         for(int z = 0; z < houses.length; z++){
             map.put(houses[z], z);
         }
-    
+    	
+	//travser all the form queries[] array
         for(int i = 0; i < queries.length; i++){
             int temp = queries[i];
             int posistion = map.get(temp);
             
             if(posistion != 0 && posistion != houses.length - 1){
                 if(houses[posistion] - 1 == houses[posistion - 1] && houses[posistion] + 1 == houses[posistion + 1]){
-                    segments += 1;
+                    segments += 1;//the segments will add 1
                 } else if (houses[posistion] - 1 != houses[posistion - 1] && houses[posistion] + 1 != houses[posistion + 1]){
-                    segments -= 1;
+                    segments -= 1; //the segments will minus 1
                 }
             } else{
                 if(posistion == 0){
                   if(houses[posistion] + 1 != houses[posistion + 1]){
-                        segments -= 1;
+                        segments -= 1; //the segments will minus 1
                    } 
                 } else if(posistion == houses.length - 1) {
                    if(houses[posistion] - 1 != houses[posistion - 1]){
-                        segments -= 1;
+                        segments -= 1; //the segments will minus 1
                    } 
                 }
             }
             
-            houses[posistion] = -1;
-            list.add(segments);
+            houses[posistion] = -1; //remove the queries houses in the array
+            list.add(segments); //add the segments into list
         }
     
         //change list to be the int[] array
