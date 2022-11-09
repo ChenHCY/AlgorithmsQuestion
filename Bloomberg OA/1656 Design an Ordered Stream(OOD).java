1cfd191225e008986f489ecb1112022ef5c363e2
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-11-08 23:04:03 -0800

    Update and rename 1656 Design an Ordered Stream.java to 1656 Design an Ordered Stream(OOD).java

--- Bloomberg OA/1656 Design an Ordered Stream(OOD).java
+++ Bloomberg OA/1656 Design an Ordered Stream(OOD).java
@@ -1,21 +1,30 @@
 /* Leetcode 1656. Design an Ordered Stream
+
 There is a stream of n (idKey, value) pairs arriving in an arbitrary order, 
 where idKey is an integer between 1 and n and value is a string. No two 
 pairs have the same id.
+
 Design a stream that returns the values in increasing order of their IDs 
 by returning a chunk (list) of values after each insertion. The concatenation 
 of all the chunks should result in a list of the sorted values.
+
 Implement the OrderedStream class:
+
 1. OrderedStream(int n) Constructs the stream to take n values.
+
 2. String[] insert(int idKey, String value) Inserts the pair (idKey, value) 
 into the stream, then returns the largest possible chunk of currently inserted 
 values that appear next in the order.
+
 Example:
+
 Input
 ["OrderedStream", "insert", "insert", "insert", "insert", "insert"]
 [[5], [3, "ccccc"], [1, "aaaaa"], [2, "bbbbb"], [5, "eeeee"], [4, "ddddd"]]
+
 Output
 [null, [], ["aaaaa"], ["bbbbb", "ccccc"], [], ["ddddd", "eeeee"]]
+
 Explanation
 // Note that the values ordered by ID is ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"].
 OrderedStream os = new OrderedStream(5);
@@ -24,9 +33,11 @@ os.insert(1, "aaaaa"); // Inserts (1, "aaaaa"), returns ["aaaaa"].
 os.insert(2, "bbbbb"); // Inserts (2, "bbbbb"), returns ["bbbbb", "ccccc"].
 os.insert(5, "eeeee"); // Inserts (5, "eeeee"), returns [].
 os.insert(4, "ddddd"); // Inserts (4, "ddddd"), returns ["ddddd", "eeeee"].
+
 // Concatentating all the chunks returned:
 // [] + ["aaaaa"] + ["bbbbb", "ccccc"] + [] + ["ddddd", "eeeee"] = ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"]
 // The resulting order is the same as the order above. 
+
 Constraints:
 1 <= n <= 1000
 1 <= id <= n
@@ -64,12 +75,6 @@ class OrderedStream {
     }
 }
 
-/**
- * Your OrderedStream object will be instantiated and called as such:
- * OrderedStream obj = new OrderedStream(n);
- * List<String> param_1 = obj.insert(idKey,value);
- */
-
 //Solution 2: Used String Array[] to solve it
 class OrderedStream {
     
@@ -90,9 +95,3 @@ class OrderedStream {
         return list;
     }
 }
-
-/**
- * Your OrderedStream object will be instantiated and called as such:
- * OrderedStream obj = new OrderedStream(n);
- * List<String> param_1 = obj.insert(idKey,value);
- */
