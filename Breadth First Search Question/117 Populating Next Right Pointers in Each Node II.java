15c5fab065fe616607c80a4bc13fc060251aa947
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-11-17 13:54:33 -0800

    Update 117 Populating Next Right Pointers in Each Node II.java

--- Breadth First Search Question/117 Populating Next Right Pointers in Each Node II.java
+++ Breadth First Search Question/117 Populating Next Right Pointers in Each Node II.java
@@ -25,3 +25,59 @@ The number of nodes in the tree is in the range [0, 6000].
 -100 <= Node.val <= 100
 */
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+//Time: O(n)   Space: O(n)
+class Solution {
+    public Node connect(Node root) {
+        if(root == null){
+            return root;
+        }
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root); //add the root first
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            
+            for(int i = 0; i < size; i++){
+                Node node = queue.poll();
+                
+                if(i < size - 1){
+                    node.next = queue.peek();
+                }
+                
+                if(node.left != null){
+                    queue.offer(node.left);
+                }
+                
+                if(node.right != null){
+                    queue.offer(node.right);
+                }
+            }
+        }
+        
+        return root;
+    }
+}
