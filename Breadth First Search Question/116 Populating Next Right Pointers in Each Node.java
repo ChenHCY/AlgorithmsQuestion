d76d9f6c9e379be5e36a2871cd31851d9ae29efd
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-11-17 14:06:04 -0800

    Update 116 Populating Next Right Pointers in Each Node.java

--- Breadth First Search Question/116 Populating Next Right Pointers in Each Node.java
+++ Breadth First Search Question/116 Populating Next Right Pointers in Each Node.java
@@ -89,3 +89,40 @@ class Solution {
         return root;
     }
 }
+
+
+//Also can Same with Leetcode 117. Populating Next Right Pointers in Each Node II
+//Time: O(n)   Space: O(n)
+class Solution {
+    public Node connect(Node root) {
+        if(root == null){
+            return root;
+        }
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root); //add the root first
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            
+            for(int i = 0; i < size; i++){
+                Node node = queue.poll();
+                
+                //
+                if(i < size - 1){
+                    node.next = queue.peek();
+                }
+                
+                if(node.left != null){
+                    queue.offer(node.left);
+                }
+                
+                if(node.right != null){
+                    queue.offer(node.right);
+                }
+            }
+        }
+        
+        return root;
+    }
+}
