934d8eed8df710422acf070fa80d8f09c1d42495
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-12-06 23:53:08 -0800

    Update 95 Unique Binary Search Trees II.java

--- Binary Search Tree/95 Unique Binary Search Trees II.java
+++ Binary Search Tree/95 Unique Binary Search Trees II.java
@@ -13,3 +13,57 @@ Output: [[1]]
 Constraints:
 1 <= n <= 8
 */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if(n == 0){
+            return new ArrayList<>();
+        }
+        return helper(1, n);
+    }
+
+    //Used the recursion method
+    public static List<TreeNode> helper(int start, int end){
+        List<TreeNode> res = new ArrayList<>();
+
+        //if there is not node value less than root(start)
+        if(start > end){
+            res.add(null); //should add the "null" value
+            return res;
+        }
+
+        //for example: n = 10 start = 1
+        for(int i = start; i <= end; i++){
+         //find the left and right list of every root node
+            List<TreeNode> leftNode = helper(start, i - 1); // null
+            List<TreeNode> rightNode = helper(i + 1, end);// 2-9
+
+            //add the value from left and right part into res list
+            for(TreeNode left : leftNode){
+                for(TreeNode right : rightNode){
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    res.add(root);
+                }
+            }
+        }
+
+        return res;
+    }
+}
