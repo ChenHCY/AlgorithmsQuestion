57ba9b831c49924e75baef39cdbfb80ce52d9230
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-12-07 00:24:39 -0800

    Update 96 Unique Binary Search Trees.java

--- Binary Search Tree/96 Unique Binary Search Trees.java
+++ Binary Search Tree/96 Unique Binary Search Trees.java
@@ -17,3 +17,18 @@ Constraints:
 1 <= n <= 19
 */
 
+//Dp Method
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 0; j < i; j++){
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        
+        return dp[dp.length - 1];
+    }
+}
