db91f203fa66d49d2d8616ce0bb78ac0f3e1bf8b
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-12-17 23:50:10 -0800

    Update 542 01 Matrix.java

--- Depth First Search Question/542 01 Matrix.java
+++ Depth First Search Question/542 01 Matrix.java
@@ -21,3 +21,28 @@ n == mat[i].length
 mat[i][j] is either 0 or 1.
 There is at least one 0 in mat.
 */
+
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int result[][] = new int[mat.length][mat[0].length];
+        for(int i=0;i<mat.length;i++) {
+            for(int j=0;j<mat[0].length;j++) {
+                if(mat[i][j]==0) {
+                    dfs(mat,result,i,j, 0);
+                }
+            }
+        }
+        return result;
+    }
+    private void dfs(int[][] mat, int[][] result, int i, int j, int currentDistance) {
+        if(i<0||j<0||i==mat.length||j==mat[0].length) 
+            return;
+        if(currentDistance ==0 || mat[i][j]==1 && (result[i][j]==0 || result[i][j] > currentDistance)) {
+            result[i][j] = currentDistance;
+            dfs(mat,result,i+1,j,currentDistance+1);
+            dfs(mat,result,i,j+1,currentDistance+1);
+            dfs(mat,result,i-1,j,currentDistance+1);
+            dfs(mat,result,i,j-1,currentDistance+1);
+        }
+    }
+}
