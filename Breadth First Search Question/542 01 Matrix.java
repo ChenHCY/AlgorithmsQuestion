8ab9c8d355c1f45e6b57ca159a4bfeb287ce10ee
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-12-25 15:45:02 -0800

    Update 542 01 Matrix.java

--- Breadth First Search Question/542 01 Matrix.java
+++ Breadth First Search Question/542 01 Matrix.java
@@ -22,38 +22,68 @@ mat[i][j] is either 0 or 1.
 There is at least one 0 in mat.
 */
 
-public class Solution {
-    public int[][] updateMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length; // m == mat.length
+        int n = mat[0].length; // n == mat[i].length
+
+        int step = 1; //step used to count the distance of the nearest 0 for each cell.
+
+        //Used for dirsearch in the 4 directions
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
+
+        //BFS Method
         Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    queue.offer(new int[] {i, j});
-                }
-                else {
-                    matrix[i][j] = Integer.MAX_VALUE;
+        //Used a boolean[][] list to save whether visted 
+        boolean[][] visited = new boolean[m][n];
+
+        //travaser all the cells 
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 0){ //when find the curr cell value is 0
+                    visited[i][j] = true; //save visisted it
+                    queue.offer(new int[]{i, j}); //save the cells into queue list
+                } else{
+                    mat[i][j] = Integer.MAX_VALUE; //change the 1-cells value to be the largest value
                 }
             }
         }
-        
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            for (int[] d : dirs) {
-                int r = cell[0] + d[0];
-                int c = cell[1] + d[1];
-             
-                if (r < 0 || r >= m || c < 0 || c >= n || matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) 
-                      continue;
-                queue.add(new int[] {r, c});
-                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
+
+        //BFS Method format while-loop to travser all the elemnt in the queue list
+        while(!queue.isEmpty()){
+            int size = queue.size(); //the cells in the current level
+
+            for(int k = 0; k < size; k++){
+                int[] curr = queue.poll(); //get the one cell from the current level
+
+                for(int[] dir : dirs){
+                    //dirsearch in the 4 directions from the current cell
+                    int row = curr[0] + dir[0];
+                    int colum = curr[1] + dir[1];
+
+                    //out the line
+                    if(row < 0 || row >= m || colum < 0 || colum >= n){
+                        continue;
+                    }
+
+                    //if cell is visited before
+                    if(visited[row][colum]){
+                        continue;
+                    }
+
+                    //if cell is not visited ===> it means the current cell is 1-cells
+                    if(!visited[row][colum]){
+                        // change the 1-cells to be the correct distance of the nearest 0 for each cell.
+                        mat[row][colum] = step; 
+                        visited[row][colum] = true; 
+                        queue.offer(new int[]{row, colum}); //add into queue again ==> move to next level
+                    }
+                }
             }
+
+            step++;
         }
-        
-        return matrix;
+
+        return mat;
     }
 }
