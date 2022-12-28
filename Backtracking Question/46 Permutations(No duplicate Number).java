ebf16b02845f758cf3f6e808146617ca2b9cf3fd
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2022-12-27 21:10:21 -0800

    Update 46 Permutations(No duplicate Number).java

--- Backtracking Question/46 Permutations(No duplicate Number).java
+++ Backtracking Question/46 Permutations(No duplicate Number).java
@@ -19,6 +19,51 @@ Constraints:
 All the integers of nums are unique.
 */
 
+
+//Solution 1: Used Backtracking Method with ArrayList contains() function
+//Time: O(n! * n)   Space: O(n)
+//If any number of recursive calls, T(n) satisfies the recurrence T(n) = T(n -1) + T(n-2) +  ... + T(1) + T(0)
+//The time will be O(n!)  ==> And add the contains() function time Q(n) ===> the total time will be O(n! * n) 
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        //Backtracking Method
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        
+        //remove 0 and null
+        if(nums.length == 0 || nums == null){
+            return res;
+        }
+        
+        helper(res, list, nums); //call backtracking method
+        
+        return res;
+    }
+    
+    //backtracking method
+    public static void helper(List<List<Integer>> res, List<Integer> list, int[] nums){
+        //Add condition
+        if(list.size() == nums.length){ //if the arraylist length = nums.length
+            res.add(new ArrayList<>(list)); //it means all the number from nums[] have ready add into arraylist
+            return; //so add the list into res arraylist
+        }
+        
+        //travser the element from nums
+        for(int i = 0; i < nums.length; i++){
+            //check whether can find number in the arraylist
+            if(list.contains(nums[i])){
+                continue; //if contains same number, continue move forward
+            }
+            list.add(nums[i]); //add the number with not contains into arraylist
+            helper(res, list, nums); //call backtracking move forward to next number in the nums[] list
+            list.remove(list.size() - 1); //backtracking method: come back in last level and number
+        }
+    }
+}
+
+******************************************************************************************************************
+//Solution 2: Used Backtracking Method without ArrayList contains() function
+//Time: O(n!)   Space: O(n)   ===========> beacuse did not use arraylist contains() function, so the time is O(n!)
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         //Backtracking Method
