7c07b2f485d9e3dafdfc25d2717de4134a52a6e8
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-01-07 00:24:30 -0800

    Update 377 Combination Sum IV.java

--- Dynamic Programming Quesrion/377 Combination Sum IV.java
+++ Dynamic Programming Quesrion/377 Combination Sum IV.java
@@ -33,3 +33,31 @@ All the elements of nums are unique.
 1 <= target <= 1000
 */
 
+//Thought: Dp method ==> to find how many combination ways that add up to in every number
+//Time: O(n * 1) Space: O(n)
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        //dp state: means how many combination ways that add up to target
+        int[] dp = new int[target + 1];
+
+        dp[0] = 1; //dp initialize
+
+        /*      Target Value:  0 1 2 3 4   nums[]: [1, 2, 3]  target = 4
+            Combination ways:  1 1 2 4 7 ==> dp[i] = dp[i - nums[0]] + dp[i - nums[1]] .... + dp[i - nums[nums.length - 1]];
+        */
+
+        for(int i = 1; i < dp.length; i++){
+            for(int num : nums){ //the combination numbers must be from nums[] array
+                if(i - num >= 0){ //the index can not be negative
+                    //it means this number can be added into the Combination that sum to the "target"
+                    dp[i] += dp[i - num]; //dp function ==> count the how many Combination ways
+                }
+            }
+        }
+
+        return dp[target]; //dp return
+    }
+}
