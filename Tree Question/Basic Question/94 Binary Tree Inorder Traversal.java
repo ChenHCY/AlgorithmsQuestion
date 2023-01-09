ff4e096fb3ddbd1db9a728ecf29832b5c91f4a23
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-01-09 02:14:34 -0800

    Create 94 Binary Tree Inorder Traversal.java

---
+++ Tree Question/Basic Question/94 Binary Tree Inorder Traversal.java
@@ -0,0 +1,43 @@
+/*Leetcode  94 Binary Tree Inorder Traversal
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+
+Input: root = []
+Output: []
+
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+//Recursive solution
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public static void helper(TreeNode root, List<Integer> list){
+        if(root == null){
+            return;
+        }
+
+        helper(root.left, list);
+        list.add(root.val);
+        helper(root.right, list);
+    }
+}
