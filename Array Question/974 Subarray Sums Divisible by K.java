ff48d062efdb396a997a0074b6b08a42530be878
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-01-19 02:31:15 -0800

    Update 974 Subarray Sums Divisible by K.java

--- Array Question/974 Subarray Sums Divisible by K.java
+++ Array Question/974 Subarray Sums Divisible by K.java
@@ -24,36 +24,35 @@ Constraints:
 2 <= k <= 104
 */
 
+ // 如果任意两个数字除以K得到的余数相同，那么两数之差一定可以被K整除。
+// 对于数组任意一个连续子数组的和sum[i1, i2](下标i1到i2之和)应该等于：
+// ==》 sum[i1, i2] = sum[0, i2] - sum[0, i1]; (前缀和的方式)
+// ==》所以nums数组中任意一个子数组的和，都可以由上面相应的2个sum之差来取得
+//==》同时根据余数定理，所有sum分别除以K，余数相同的sum之差是可以被K整除的
+// ==》这题的形式就换成了：寻找余数相同的个数 ==》 用HashMAp来存储每个数字对应的余数
+
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>(); // 记录相同余数个数的map
-        int sum = 0;
-        int count = 0;
-        
-        map.put(0, 1);
-        
-        // 如果任意两个数字除以K得到的余数相同，那么两数之差一定可以被K整除。
-        // 对于数组任意一个连续子数组的和sum[i1, i2](下标i1到i2之和)应该等于：
-        // ==》 sum[i1, i2] = sum[0, i2] - sum[0, i1]; (前缀和的方式)
-        // ==》所以nums数组中任意一个子数组的和，都可以由上面相应的2个sum之差来取得
-        //==》同时根据余数定理，所有sum分别除以K，余数相同的sum之差是可以被K整除的
-        // ==》这题的形式就换成了：寻找余数相同的个数 ==》 用HashMAp来存储每个数字对应的余数
+     
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0; // used for count the remainder of every element in nums[] array
+        int count = 0; // count the how many subarrays sums divisibe by k
 
-        //travser all the element from nums
+        map.put(0, 1); //if the remainder is 0, it means the subarrays that have a sum divisible by k
+
         for(int num : nums){
-            // 当前sum的余数
-            sum = (sum + num) % k; //calculate whether the sum for current range can divisible by k.
-            //if the sum is negative ==》 // 如果余数小于0，转化为正数
-            if(sum < 0){
-                sum += k; //change back to the positive
+            sum = (sum + num) % k; // get the remainder(余数) of every element in nums[] array
+
+            if(sum < 0){ //if sum is less than 0
+                sum += k; //change the negative to be positive
             }
-            
-            //从map中取得之前相同余数的个数
-            count += map.getOrDefault(sum, 0); 
-            // 更新map中该余数的个数
-            map.put(sum, map.getOrDefault(sum, 0) + 1); //put into the hashmap and save with the sum times
+
+            //同时根据余数定理，所有sum分别除以K，余数相同的sum 都是可以被K整除的
+            count += map.getOrDefault(sum, 0); //check and find how many numbers of the value with same remainder
+            //renew the hashmap numbers
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-        
+
         return count;
     }
 }
