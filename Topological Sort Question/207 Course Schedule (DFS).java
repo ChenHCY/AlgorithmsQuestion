7d0d2fd09b0e9cd5138fab54c78a9743a28e8f42
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-02-02 00:21:17 -0800

    Update and rename 207 Course Schedule.java to 207 Course Schedule (DFS).java

--- Topological Sort Question/207 Course Schedule (DFS).java
+++ Topological Sort Question/207 Course Schedule (DFS).java
@@ -32,9 +32,28 @@ prerequisites[i].length == 2
 All the pairs prerequisites[i] are unique.
 */
 
+/*
+
+思路：其实这个题目就是检查一个DAG图中 是否存在环，如果有 则代表无法完成全部课程
+
+"如果发现这幅有向图中存在环，那就说明课程之间存在循环依赖，肯定没办法全部上完；反之，如果没有环，那么肯定能上完全部课程。"
+
+1. 那首先第一步：构建一个有向图 ==》 这种有向图的构建 通常使用的是邻接表，比如下面这种结构：List<Integer>[] graph; ==》 graph[s] 是一个列表，存储着节点 s 所指向的节点。
+
+然后我就得到了一张图，包括每个节点指向的的下一个节点： graph[currCourse].add(nextCourse)
+
+==》 for example: graph[0].add(1); graph[0].add(2) 表示 可以通过 0 指向 1 和 2（0 是 1 2 的前缀课）
+
+2. 然后第二步：遍历所有节点（课程），来检查是否存在 “环”
+
+=》 注意图中并不是所有节点都相连，所以要用一个 for 循环将所有节点都作为起点调用一次 DFS 搜索算法。
+
+*** 核心就是判断一幅有向图中是否存在环。
+*/
+
 class Solution {
-    boolean[] visited; //save the nodes has visited before
-    boolean[] onePath; // save the traverse path
+    boolean[] visited; //save the nodes has visited before 这个只记访问过的节点
+    boolean[] onePath; // save the traverse path 这个记节点是否在之前的路径上
     boolean hasCycle = false; // save whether the graph have cycle
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
@@ -46,6 +65,7 @@ class Solution {
         onePath = new boolean[numCourses];
 
         //travserse all the node from numCourses
+       //因为图中并不是所有节点都相连，所以要用一个 for 循环将所有节点都作为起点调用一次 DFS 搜索算法。
         for(int i = 0; i < numCourses; i++){
             traverse(graph, i);
         }
@@ -57,7 +77,8 @@ class Solution {
         return true; //if did not have any cycle ==> return true
     }
 
-    //DFS function: traverse all the elements from prerequisites[][] and check whether have cycle
+    //DFS function: traverse all the elements from prerequisites[][] and check whether have cycle 
+  // 使用DFS来判断图中是否存在环。
     public void traverse(List<Integer>[] graph, int currNode){
         //the exit condition ==> meet the node has saved the pervious path before
         if(onePath[currNode]){
@@ -70,7 +91,8 @@ class Solution {
         }
 
         //save the curr node has visited it and the path of node
-        visited[currNode] = true;
+        //回溯算法的道理： 在进入每个节点的时候将 onPath[节点] visited[节点]标记为 true，
+        visited[currNode] = true; 
         onePath[currNode] = true;
 
         //move to the next node of the graph path
@@ -78,11 +100,12 @@ class Solution {
             traverse(graph, nextNode);
         }
 
-        //change back to false ==> backtracking
+        //change back to false ==> backtracking，离开的时候改变onPath[节点] 回去false 
+        // 这样如果发现 onPath[s] 已经被标记true，就说明出现了环。
         onePath[currNode] = false;
     }
 
-    //the function to used build graph
+    //the function to used build graph 构建一个有向图
     public List<Integer>[] buildGraph(int numCourses, int[][] prerequisites) {
         //邻接表 ==> graph[s] 是一个列表，存储着节点 s 所指向的节点。
         List<Integer>[] graph = new LinkedList[numCourses]; 
