857f6f8283ea7e0185f346708812a51aee6a2077
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-05-30 18:28:55 -0700

    Create 705 Design HashSet.java

---
+++ HashMap Question/HashSet Question/705 Design HashSet.java
@@ -0,0 +1,67 @@
+/* Leetcode 705. Design HashSet
+
+Design a HashSet without using any built-in hash table libraries.
+
+Implement MyHashSet class:
+
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ 
+Example 1:
+
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
+
+Explanation
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1);      // set = [1]
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(1); // return True
+myHashSet.contains(3); // return False, (not found)
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(2); // return True
+myHashSet.remove(2);   // set = [1]
+myHashSet.contains(2); // return False, (already removed)
+ 
+
+Constraints:
+
+0 <= key <= 106
+At most 104 calls will be made to add, remove, and contains.
+*/
+
+class MyHashSet {
+    boolean[] number = new boolean[1000009];
+
+    public MyHashSet() {
+
+    }
+    
+    public void add(int key) {
+        number[key] = true;
+    }
+    
+    public void remove(int key) {
+        number[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        //if the hashset have this key number
+        if(number[key] == true){
+            return true;
+        }
+        return false; //the hashset did not have it
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
