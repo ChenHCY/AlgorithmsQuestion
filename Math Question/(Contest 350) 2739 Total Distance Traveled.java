4327791c97a32019d2e3940bbe6a1b2821476cab
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-06-17 21:08:54 -0700

    Update and rename 2739 Total Distance Traveled.java to (Contest 350) 2739 Total Distance Traveled.java

--- Math Question/(Contest 350) 2739 Total Distance Traveled.java
+++ Math Question/(Contest 350) 2739 Total Distance Traveled.java
@@ -1,4 +1,4 @@
-/* Leetcode 2739. Total Distance Traveled
+/* (Contest 350) Leetcode 2739. Total Distance Traveled
 A truck has two fuel tanks. You are given two integers, mainTank representing the fuel present in the main tank in liters and additionalTank representing the fuel present in the additional tank in liters.
 
 The truck has a mileage of 10 km per liter. Whenever 5 liters of fuel get used up in the main tank, if the additional tank has at least 1 liters of fuel, 
