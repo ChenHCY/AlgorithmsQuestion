7e43cc218117a2c98393384d9ec77116452fe908
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-08-09 19:35:47 -0700

    Update 365 Water and Jug Problem.java

--- Recursion Questions/365 Water and Jug Problem.java
+++ Recursion Questions/365 Water and Jug Problem.java
@@ -33,4 +33,31 @@ Constraints:
 
 1 <= jug1Capacity, jug2Capacity, targetCapacity <= 10^6
 */
+//时间复杂度：O(log⁡(min⁡(x,y))) 取决于计算最大公约数所使用的辗转相除法。
+//空间复杂度： O（1）
+class Solution {
+    // 我们可以认为每次操作只会给水的总量带来 jug1Capacity 或者 jug2Capacity 的变化量。
+    // 因此我们的目标可以改写成：找到一对整数 a,b， ==》 使得 ax + by = z 
+    // ==> 贝祖定理: 对于任何整数 a,b,m 关于未知数x和y的线性方程 ax + by = z，只存在整数解 m 是 a,b最大公约数
+    public boolean canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+        if(jug1Capacity + jug2Capacity < targetCapacity){
+            return false;
+        }
 
+        if(jug1Capacity == targetCapacity || jug2Capacity == targetCapacity || jug1Capacity + jug2Capacity == targetCapacity){
+            return true;
+        }
+
+        return targetCapacity % gcd(jug1Capacity, jug2Capacity) == 0;
+    }
+
+    private int gcd(int x, int y){
+        int temp = x % y;
+        while(temp != 0){
+            x = y;
+            y = temp;
+            temp = x % y;
+        }
+        return y;
+    }
+}
