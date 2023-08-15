bc44c59381af35cff4c7e3771ca473c91f8db0d9
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-08-14 22:55:00 -0700

    Create 469 Same Tree.java

---
+++ Tiktok Lintcode Interview/469 Same Tree.java
@@ -0,0 +1,63 @@
+/*Lintcode 469 · Same Tree
+
+Check if two binary trees are identical. Identical means the two binary trees have the same structure and every identical position has the same value.
+
+Example 1:
+
+Input:{1,2,2,4},{1,2,2,4}
+Output:true
+Explanation:
+        1                   1
+       / \                 / \
+      2   2   and         2   2
+     /                   /
+    4                   4
+
+are identical.
+Example 2:
+
+Input:{1,2,3,4},{1,2,3,#,4}
+Output:false
+Explanation:
+
+        1                  1
+       / \                / \
+      2   3   and        2   3
+     /                        \
+    4                          4
+
+are not identical.
+
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param a: the root of binary tree a.
+     * @param b: the root of binary tree b.
+     * @return: true if they are identical, or false.
+     */
+    public boolean isIdentical(TreeNode a, TreeNode b) {
+        // write your code here
+        if(a == null && b == null){
+            return true;
+        } else if(a == null || b == null){
+            return false;
+        } else if(a.val != b.val){
+            return false;
+        } else {
+            return isIdentical(a.left, b.left) && isIdentical(a.right, b.right);
+        }
+    }
+}
