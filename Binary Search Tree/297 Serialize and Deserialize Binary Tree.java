85a61b4e275e5c7be6b3c47dc0696114a61dbd83
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-10-25 02:18:56 -0700

    Update 297 Serialize and Deserialize Binary Tree.java

--- Binary Search Tree/297 Serialize and Deserialize Binary Tree.java
+++ Binary Search Tree/297 Serialize and Deserialize Binary Tree.java
@@ -24,22 +24,15 @@ The number of nodes in the tree is in the range [0, 104].
 */
 
 /*解题思路
-序列化：把一个二叉树root转化为一个String的字符串，所以需要使用BFS的变形。把每一层加入Dueue list中，然后再放入StringBuilder中。
+序列化：把一个二叉树root转化为一个String的字符串，
+  ==》 所以需要使用BFS的变形。把每一层加入 Queue list中，然后再放入 StringBuilder中。
 
 反序列化：把一个String 字符串转化为一个二叉树root, 所以也是BFS的变形。
     1. String[] str = data.split(" "); 把String变为一个String Array
-    2. for-loop循环String[] Array所有值，然后BFS一层一层的加入到root 二叉树里面对应的点位
+    2. for-loop循环String[] Array所有值，然后BFS一层一层的加入到 Queue中进行组装
+    3. 从Queue中提取节点值，进行组装tree，==> 把这些值加入到 root二叉树 里面对应的点位
 */
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Codec {
 
     // Encodes a tree to a single string.
@@ -90,12 +83,12 @@ public class Codec {
         TreeNode root = new TreeNode(Integer.parseInt(str[0]));
         //BFS method list
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+        queue.offer(root); //把root根节点加入到queue队列中，进行组装tree
 
         //for-loop travser all the element from String data
         for(int i = 1; i < str.length; i++){
             //get the first node from Queue list
-            TreeNode node = queue.poll();
+            TreeNode node = queue.poll(); //首先得到的就是根节点root, 然后一层一层组装
             //if there can correct find node value
 
             if(!str[i].equals("null")){
@@ -107,7 +100,7 @@ public class Codec {
             i++; //move to check right side
 
             if(!str[i].equals("null")){
-                //add the value into left node of current node
+                //add the value into right node of current node
                 node.right = new TreeNode(Integer.parseInt(str[i]));
                 queue.offer(node.right);
             }
@@ -120,4 +113,3 @@ public class Codec {
 // Codec ser = new Codec();
 // Codec deser = new Codec();
 // TreeNode ans = deser.deserialize(ser.serialize(root));
- 
