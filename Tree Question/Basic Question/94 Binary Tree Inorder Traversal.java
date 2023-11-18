e73fcf7bfb3f3c487e429265b888d2959b844e3f
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-11-17 22:13:49 -0800

    Update 94 Binary Tree Inorder Traversal.java

--- Tree Question/Basic Question/94 Binary Tree Inorder Traversal.java
+++ Tree Question/Basic Question/94 Binary Tree Inorder Traversal.java
@@ -23,21 +23,45 @@ The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 */
 
-//Recursive solution
+//Solution 1: Recursive solution 递归 （类似于 DFS）
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        helper(root, res);
+        dfsRecursion(root, res);
         return res;
     }
-
-    public static void helper(TreeNode root, List<Integer> list){
-        if(root == null){
+    public static void dfsRecursion(TreeNode node, List<Integer> list){
+        //exit condition
+        if(node == null){
             return;
         }
+        //中序遍历：左子数 -> 根节点 -> 右子树
+        dfsRecursion(node.left, list);
+        list.add(node.val);
+        dfsRecursion(node.right, list);
+    }
+}
 
-        helper(root.left, list);
-        list.add(root.val);
-        helper(root.right, list);
+
+// Solution 2: iteratively 迭代 （类似于 BFS）
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode currNode = root;
+
+        //中序遍历：从最左边的节点开始添加，左子树 -> 根节点 -> 右子树
+        while(!stack.isEmpty() || currNode != null){
+            // 首先找到最左边的节点，把路径上的节点添加进队列中
+            while(currNode != null){
+                stack.addLast(currNode);
+                currNode = currNode.left; 
+            }
+            currNode = stack.pollLast();
+            res.add(currNode.val);
+            currNode = currNode.right;
+        }
+
+        return res;
     }
 }
