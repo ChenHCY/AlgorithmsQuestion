7bc6e7c91d0acdf51dca2e4e034393ed31c4ea80
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2023-11-21 19:15:01 -0800

    Create 176 Second Highest Salary.sql

---
+++ Backend Leetcode/176 Second Highest Salary.sql
@@ -0,0 +1,55 @@
+/* Leetcode 176. Second Highest Salary
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+Example 2:
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+*/
+
+# Write your MySQL query statement below
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary NOT IN ( # 跳过where包含的值 ==> 也就是找到除最高薪水以外的 最大值 (第二高薪水)
+    SELECT MAX(Salary) FROM Employee # 最高薪水
+)
